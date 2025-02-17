cadb6a6a750295e8612cebcffcbf99c2a7efd58e
kpisti18 <k.istvanzoltan@gmail.com>
2025-02-17 12:38:54 +0100

    fetc_gyakorlas

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', fetchCharacters);
+
+async function fetchCharacters() {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
+        console.log(response);
+        
+    } catch (error) {
+        
+    }
+}
